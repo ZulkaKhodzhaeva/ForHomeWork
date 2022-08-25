6055f97413de824f8453545528f0d46466f5d1e1
Honey <NNNNNioo@gmail.com>
2022-08-25 21:51:59 +0300

    Решение задачи 23

---
+++ task_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напшите программу, которая принимает на вход число (N) и 
+// выдает таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите любое положительное число: ");
+int newnumb = int.Parse(Console.ReadLine());
+int count = 1;
+
+if(newnumb < 0)
+{
+    Console.WriteLine("Введенное значение отрицательное");
+}
+else
+{
+    while(count <= newnumb)
+    {
+        double cube = Math.Pow(per, 3);
+        Console.Write($"{par} ");
+        per++;
+    }
+}
