32a1d1adb93946dd9e1b1bf9600ac8e1d6d2acda
Honey <NNNNNioo@gmail.com>
2022-09-14 12:48:16 +0300

    Решение задач 47 и 50

---
+++ lesson_7/task_47/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] TwoRealArray(int row, int line, int arg1)
+{
+    double[,] newTwoRealArray = new double[row, line];
+    for (int i = 0; i < newTwoRealArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newTwoRealArray.GetLength(1); j++)
+        {
+            newTwoRealArray[i, j] = new Random().NextDouble() * 2 - 1;
+            newTwoRealArray[i, j] *= arg1; 
+        }
+    }
+    return newTwoRealArray;
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]:f2}\t      ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter count of rows m: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Enter count of lines n: ");
+int lines = int.Parse(Console.ReadLine());
+Console.Write("Enter count of variable: ");
+int var = int.Parse(Console.ReadLine());
+
+double[,] arrayTemp = TwoRealArray(rows, lines, var);
+Console.WriteLine();
+PrintArray(arrayTemp);
+
---
+++ lesson_7/task_50/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число и генерирует случайный двумерный массив, 
+// и возвращает индексы этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] TwoIntArray(int beginArray, int endArray, int row, int line)  
+{
+    int[,] newTwoRealArray = new int[row, line];
+    for (int i = 0; i < newTwoRealArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newTwoRealArray.GetLength(1); j++)
+        {
+            newTwoRealArray[i, j] = new Random().Next(beginArray, endArray);
+        }
+    }
+    return newTwoRealArray;
+}
+
+void PrintArray1(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SearchPosition(int[,] array, int numbers)
+{
+    int count = 0;
+    int countPosition = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == numbers)
+            {
+        
+                Console.WriteLine($"Position {countPosition} found element have to index ({i}, {j});");
+                countPosition++;
+                count++;
+            }
+        }
+
+    }
+    if (count > 0) return;
+    else Console.WriteLine("Element not in array");
+}
+
+Console.Write("Enter count of rows: ");
+int rowsArray = int.Parse(Console.ReadLine());
+Console.Write("Enter count of lines: ");
+int linesArray = int.Parse(Console.ReadLine());
+Console.Write("Enter beginning of the segment: ");
+int beginSegment = int.Parse(Console.ReadLine());
+Console.Write("Enter end of the segment: ");
+int endSegment = int.Parse(Console.ReadLine());
+Console.WriteLine("********************************************");
+
+int[,] newArrayMatrix = TwoIntArray(beginSegment, endSegment, rowsArray, linesArray);
+PrintArray1(newArrayMatrix);
+
+Console.WriteLine("********************************************");
+Console.Write("Enter a number to search for: ");
+int searchNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("********************************************");
+SearchPosition(newArrayMatrix, searchNumber);
+
