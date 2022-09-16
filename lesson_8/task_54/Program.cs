df8a253b34426f0638d8eea63adde890d82fb323
Honey <NNNNNioo@gmail.com>
2022-09-16 21:00:11 +0300

    Решение задач 54 и 56

---
+++ lesson_8/task_54/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] BiArray(int beginArray, int endArray, int row, int line)
+{
+    int[,] newTwoRealArray = new int[row, line];
+    for (int i = 0; i < newTwoRealArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newTwoRealArray.GetLength(1); j++)
+        {
+            newTwoRealArray[i, j] = new Random().Next(beginArray, endArray);
+        }
+    }
+    return newTwoRealArray;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortBiArray(int[,] biArray)
+{
+    for (int i = 0; i < biArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < biArray.GetLength(1) - 1; j++)
+        {
+            int maxPos = j;
+            for (int k = j + 1; k < biArray.GetLength(1); k++)
+            {
+                if (biArray[i, k] > biArray[i, maxPos]) maxPos = k;
+            }
+            int maxTempInArray = biArray[i, j];
+            biArray[i, j] = biArray[i, maxPos];
+            biArray[i, maxPos] = maxTempInArray;
+        }
+    }
+    return biArray;
+}
+
+Console.Write("Enter count of rows: ");
+int rowsArray = int.Parse(Console.ReadLine());
+Console.Write("Enter count of lines: ");
+int linesArray = int.Parse(Console.ReadLine());
+Console.Write("Enter beginning of the segment: ");
+int beginSegment = int.Parse(Console.ReadLine());
+Console.Write("Enter end of the segment: ");
+int endSegment = int.Parse(Console.ReadLine());
+Console.WriteLine("********************************************");
+
+int[,] newArrayMatrix = BiArray(beginSegment, endSegment, linesArray, rowsArray);
+PrintArray(newArrayMatrix);
+Console.WriteLine("********************************************");
+int[,] newArrayMatrix1 = SortBiArray(newArrayMatrix);
+PrintArray(newArrayMatrix1);
+
+
+
+
+
+
---
+++ lesson_8/task_56/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] BiArray(int beginArray, int endArray, int row, int line)
+{
+    int[,] newTwoRealArray = new int[row, line];
+    for (int i = 0; i < newTwoRealArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newTwoRealArray.GetLength(1); j++)
+        {
+            newTwoRealArray[i, j] = new Random().Next(beginArray, endArray);
+        }
+    }
+    return newTwoRealArray;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMinSumLinesBiArray(int[,] biArray)
+{
+
+    int sumLines = 0;
+    int count = 0;
+    int sumFirstLines = 0;
+    for (int k = 0; k < biArray.GetLength(1); k++)
+    {
+        sumFirstLines += biArray[0, k];
+    }
+    for (int i = 0; i < biArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < biArray.GetLength(1); j++)
+        {
+            sumLines += biArray[i, j];
+        }
+        Console.WriteLine($"Sum lines {i+1} = {sumLines} ");
+        if (sumFirstLines > sumLines)
+        {
+            sumFirstLines = sumLines;
+            count = i;
+        }
+        sumLines = 0;
+    }
+    Console.WriteLine("********************************************");
+    Console.WriteLine($"Count lines with min sum elements = {count + 1}");
+}
+
+
+Console.Write("Enter count of rows: ");
+int rowsArray = int.Parse(Console.ReadLine());
+Console.Write("Enter count of lines != rows: ");
+int linesArray = int.Parse(Console.ReadLine());
+if (rowsArray == linesArray)
+{
+    Console.WriteLine("Введите значения не равные друг другу");
+    return;
+}
+Console.Write("Enter beginning of the segment: ");
+int beginSegment = int.Parse(Console.ReadLine());
+Console.Write("Enter end of the segment: ");
+int endSegment = int.Parse(Console.ReadLine());
+Console.WriteLine("********************************************");
+
+int[,] newArrayMatrix = BiArray(beginSegment, endSegment, linesArray, rowsArray);
+PrintArray(newArrayMatrix);
+Console.WriteLine("********************************************");
+
+SortMinSumLinesBiArray(newArrayMatrix);
+
+
+
+
