624796e1595745789c011505f9e4bbf54a0c0e6c
Honey <NNNNNioo@gmail.com>
2022-09-18 19:06:12 +0300

    Решение задач 66 и 68

---
+++ lesson_9/task_66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumElementInSegment(int m, int n)
+{
+    if (m > n) return 0;
+    return m + SumElementInSegment(++m, n);
+}
+
+int SumElementInSegmentTiny(int m, int n)
+{
+    return m > n ? 0 : m + SumElementInSegment(++m, n);
+}
+
+
+Console.Write("Enter the number m: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Enter the number n: ");
+int numN = int.Parse(Console.ReadLine());
+int sumNum = SumElementInSegmentTiny(numM, numN);
+Console.WriteLine("-----------------------------");
+Console.Write($"Sum numbers in the segment = {sumNum}.");
---
+++ lesson_9/task_68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+int Akrm(int num, int arg)
+{
+    if (num == 0) return arg + 1;
+    else if (num > 0 & arg == 0) return Akrm(num - 1 , 1);
+    else return Akrm(num - 1, Akrm(num, arg - 1));
+}
+
+Console.Write("Enter the number m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter the argument n: ");
+int n = int.Parse(Console.ReadLine());
+
+int akkerman = Akrm(m, n);
+Console.WriteLine($"The Akkerman function numbers {m} and arguments {n} = {akkerman}");
